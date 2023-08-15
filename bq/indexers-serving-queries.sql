ff54b070f8131617bd2127fda23b4740811f6641
Alex Pakalniskis <43630382+alex-pakalniskis@users.noreply.github.com>
2023-08-15 16:43:06 -0400

    Update and rename indexers-serving-queries.sql to indexers-rewards.sql

--- bq/indexers-serving-queries.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-  FORMAT_DATE('%b-%Y', DATE(transaction_created_at)) AS mon_year, 
-  COUNT(DISTINCT(indexer)) AS `indexer_count`,
-  SUM(query_fees_collected) AS `sum_query_fees_collected`,
-  SUM(query_fee_rebates) AS `sum_query_fee_rebates`
-FROM `graph-mainnet.internal_metrics.metrics_indexer_allocations` 
-WHERE `transaction_created_at` >= CAST('2022-01-01T00:00:00.000000' AS TIMESTAMP)
-GROUP BY mon_year
-ORDER BY PARSE_DATE('%b-%Y', mon_year)  
