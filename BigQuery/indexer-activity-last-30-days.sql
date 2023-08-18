e22e0a3322331cc5e8b48bc2fb95dab5b1893db7
Alex Pakalniskis <43630382+alex-pakalniskis@users.noreply.github.com>
2023-08-18 11:44:36 -0400

    Create indexer-activity-last-30-days.sql

---
+++ BigQuery/indexer-activity-last-30-days.sql
@@ -0,0 +1,18 @@
+SELECT 
+  indexer,
+  COUNT(DISTINCT(subgraph_deployment)) AS `count_distinct_subgraph_deployment`,
+  COUNT(DISTINCT(transaction_id)) AS `count_distinct_transaction_id`,
+  COUNT(DISTINCT(transaction_id)) - COUNT(DISTINCT(subgraph_deployment)) AS `delta_transactions_subgraphs`,
+  SUM(query_fees_collected) AS `sum_query_fees_collected`,
+  SUM(query_fee_rebates) AS `sum_query_fee_rebates`,
+  SUM(indexing_rewards) AS `sum_indexing_rewards`,
+  SUM(allocated_tokens) AS `sum_allocated_tokens`
+FROM 
+  `graph-mainnet.internal_metrics.metrics_indexer_allocations` 
+WHERE 
+  `transaction_created_at` >= CAST(DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY) AS TIMESTAMP)
+GROUP BY
+  indexer
+ORDER BY
+  delta_transactions_subgraphs
+  DESC
